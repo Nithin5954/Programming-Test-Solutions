071f9d02faea097a60304d7bf41b8ae45552a5c8
Nithin5954 <nithinnayak4444@gmail.com>
2025-12-01 13:25:32 +0530

    Implement odd number printer based on user input
    This program takes an integer input and prints all odd numbers up to that integer. If the input is even, it prints odd numbers up to the input minus one.

---
+++ Program3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Program3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        int limit;
+
+        if (a % 2 == 0) {
+            limit = a - 1;    
+        } else {
+            limit = a;
+        }
+
+        for (int i = 1; i <= limit; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
